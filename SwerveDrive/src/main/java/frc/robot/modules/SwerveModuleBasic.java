dd7a7f2c7e06676f29d7e4cae3874a24cfded01d
CalT128 <astarfall1288@gmail.com>
2022-10-18 21:08:43 -0400

    10/18

---
+++ SwerveDrive/src/main/java/frc/robot/modules/SwerveModuleBasic.java
@@ -0,0 +1,9 @@
+package frc.robot.modules;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+public class SwerveModuleBasic {
+    WPI_TalonFX driveMotor;
+    WPI_TalonFX turnMotor;
+    public SwerveModuleBasic(int driveMotorID, int turnMotorID, boolean driveMotorReversed, boolean turnMotorReversed){
+
+    }
+}
