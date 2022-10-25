2681948f5606cb799d5db8ffc9d1f93bbd49c0da
henrodorito <92941727+CalT128@users.noreply.github.com>
2022-10-25 15:15:20 -0400

    Swerve 10/25

--- SwerveDrive/src/main/java/frc/robot/modules/SwerveModuleBasic.java
+++ SwerveDrive/src/main/java/frc/robot/modules/SwerveModuleBasic.java
@@ -1,9 +1,19 @@
 package frc.robot.modules;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 public class SwerveModuleBasic {
     WPI_TalonFX driveMotor;
     WPI_TalonFX turnMotor;
+    boolean driveMotorReversed;
+    boolean turnMotorReversed;
     public SwerveModuleBasic(int driveMotorID, int turnMotorID, boolean driveMotorReversed, boolean turnMotorReversed){
-
+        driveMotor = new WPI_TalonFX(driveMotorID);
+        turnMotor = new WPI_TalonFX(turnMotorID);
+        this.driveMotorReversed = driveMotorReversed;
+        this.turnMotorReversed = turnMotorReversed;
+    }
+    public void drive(double speed, double rotation){
+        driveMotor.set(ControlMode.PercentOutput,speed);
+        turnMotor.set(ControlMode.PercentOutput,rotation);
     }
 }
--- SwerveDrive/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ SwerveDrive/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,14 +4,34 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.modules.SwerveModuleBasic;
+import frc.robot.Constants.SwerveDrive;
 
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {}
+  SwerveModuleBasic frontLeftModule, frontRightModule, backLeftModule, backRightModule;
+  public SwerveSubsystem() {
+    frontLeftModule = new SwerveModuleBasic(SwerveDrive.FRONT_LEFT_MOTOR,SwerveDrive.FRONT_LEFT_ROTATION,false,false);
+    frontRightModule = new SwerveModuleBasic(SwerveDrive.FRONT_RIGHT_MOTOR,SwerveDrive.FRONT_RIGHT_ROTATION,false,false);
+    backLeftModule = new SwerveModuleBasic(SwerveDrive.BACK_LEFT_MOTOR,SwerveDrive.BACK_LEFT_ROTATION,false,false);
+    backRightModule = new SwerveModuleBasic(SwerveDrive.BACK_RIGHT_MOTOR,SwerveDrive.BACK_RIGHT_ROTATION,false,false);
+  }
+  public void drive(double speed,double rotation){
+    frontLeftModule.drive(speed,rotation);
+    frontRightModule.drive(speed,rotation);
+    backLeftModule.drive(speed,rotation);
+    backRightModule.drive(speed,rotation);
+  }
+  public void setZero(){
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  
 }
