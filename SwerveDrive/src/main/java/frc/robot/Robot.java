a423b2e0b8f567ef7ddbf8dc214542d9e007c314
henrodorito <92941727+CalT128@users.noreply.github.com>
2022-11-29 15:45:55 -0500

    11/29

--- SwerveDrive/src/main/java/frc/robot/Robot.java
+++ SwerveDrive/src/main/java/frc/robot/Robot.java
@@ -71,6 +71,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    m_teleopCommand = m_robotContainer.getTeleopCommand(); 
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
@@ -78,7 +79,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    m_teleopCommand = m_robotContainer.getTeleopCommand(); 
+    m_teleopCommand.schedule();
+    
   }
 
   /** This function is called periodically during operator control. */
